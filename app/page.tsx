b369ee46ff58e1db552a5a2290b62f95318b8543
Tony Qu <tonyqug@gmail.com>
2025-08-21 19:20:09 -0400

    boardpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx
 'use client'
 
-import DashboardPage from '@/components/dashboard/dashboardPage'
+import DashboardPage from '@/components/dashboard/boardpage'
 
 export default function Page() {
   return <DashboardPage />
